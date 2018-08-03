6f3111f3ee7a7b6af10e984a8a6292fe3bc1fb6e
Weerawat Pleanchum <TomMacbookpro@macbook-pro-khxng-weerawat.local>
2018-08-03 15:41:34 +0700

    Initial Commit

---
+++ FirstIosApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstIosApp
+//
+//  Created by Weerawat Pleanchum on 3/8/2561 BE.
+//  Copyright © 2561 Tom Weerawat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
