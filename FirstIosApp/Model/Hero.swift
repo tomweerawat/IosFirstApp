d391fcf66fc6b2259e2ea97516e2c8b8ee5255fc
Weerawat Pleanchum <TomMacbookpro@MacBook-Pro-khxng-Weerawat.local>
2018-09-15 22:55:52 +0700

    load success

---
+++ FirstIosApp/Model/Hero.swift
@@ -0,0 +1,22 @@
+//
+//  Hero.swift
+//  FirstIosApp
+//
+//  Created by Weerawat Pleanchum on 15/9/2561 BE.
+//  Copyright © 2561 Tom Weerawat. All rights reserved.
+//
+
+import UIKit
+
+class Hero {
+
+    var name: String?
+    var team: String?
+    var imageUrl: String?
+    
+    init(name: String?, team: String?, imageUrl: String?) {
+        self.name = name
+        self.team = team
+        self.imageUrl = imageUrl
+    }
+}
--- FirstIosApp/SpotsViewController.swift
+++ FirstIosApp/SpotsViewController.swift
@@ -9,36 +9,88 @@
 import UIKit
 import MapKit
 import Alamofire
+import AlamofireImage
 class SpotsViewController: UITableViewController {
     enum Constants {
         static let baseURLPath = "https://nuuneoi.com/courses/500px/list"
         
     }
+    @IBOutlet var tableViewLoad: UITableView!
     var vacationSpots = [VacationSpot]()
-    
+    var heroes = [Hero]()
+    let URL_GET_DATA = "https://simplifiedcoding.net/demos/marvel/"
     // MARK: - Lifecycle
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        vacationSpots = VacationSpot.loadAllVacationSpots()
+//        vacationSpots = VacationSpot.loadAllVacationSpots()
+         loadHero()
+      
         
         
     }
+    override func viewDidLoad() {
+         super.viewDidLoad()
+       
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return vacationSpots.count
+        return heroes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "VacationSpotCell", for: indexPath) as! VacationSpotCell
-        let vacationSpot = vacationSpots[indexPath.row]
-        cell.nameLabel.text = vacationSpot.name
-        cell.locationNameLabel.text = vacationSpot.locationName
-        cell.thumbnailImageView.image = UIImage(named: vacationSpot.thumbnailName)
+//        let vacationSpot = vacationSpots[indexPath.row]
+//        cell.nameLabel.text = vacationSpot.name
+//        cell.locationNameLabel.text = vacationSpot.locationName
+//        cell.thumbnailImageView.image = UIImage(named: vacationSpot.thumbnailName)
+        let hero: Hero
+        hero = heroes[indexPath.row]
+        
+        cell.nameLabel.text = hero.name
+        cell.locationNameLabel.text = hero.team
+      
+        Alamofire.request(hero.imageUrl!).responseImage { response in
+            debugPrint(response)
+            
+            if let image = response.result.value {
+                cell.thumbnailImageView.image = image
+                
+            }
+        }
+        
+        
         
         return cell
     }
 
+    func loadHero(){
+        //fetching data from web api
+        Alamofire.request(URL_GET_DATA).responseJSON { response in
+            
+            //getting json
+            if let json = response.result.value {
+                
+                //converting json to NSArray
+                let heroesArray : NSArray  = json as! NSArray
+                
+                //traversing through all elements of the array
+                for i in 0..<heroesArray.count{
+                    
+                    //adding hero values to the hero list
+                    self.heroes.append(Hero(
+                        name: (heroesArray[i] as AnyObject).value(forKey: "name") as? String,
+                        team: (heroesArray[i] as AnyObject).value(forKey: "team") as? String,
+                        imageUrl: (heroesArray[i] as AnyObject).value(forKey: "imageurl") as? String
+                    ))
+                    
+                }
+                self.tableViewLoad.reloadData()
+            }
+            
+        }
+        
+    }
     func loadPhoto(){
         Alamofire.request(Constants.baseURLPath).responseJSON{response in
             //            print(response)
